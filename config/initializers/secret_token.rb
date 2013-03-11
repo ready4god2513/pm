fd0afce90905de05c599cf423cc18c2f13ad2551
Brandon Hansen <brandonh@ibethel.org>
2013-03-11 10:55:22 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomadpm::Application.config.secret_token = '3287393174a8c6e468e334e8a05e19c14749030573e1df74da613bc26306e9e9bcb6a2b72e0f25575338860d2e852335faab27be402eef9fd01d9a3057ca74b2'
