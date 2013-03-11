8d39bb2e82eb5e7cc98289b641e2f1a6ccb5c55e
Brandon Hansen <brandonh@ibethel.org>
2013-03-11 11:38:06 -0700

    Update airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
   config.host = 'bethel-errors.herokuapp.com'
-  config.api_key = '04c08789f3750065e57725b8177abbed'
+  config.api_key = 'a45414db94ec94e14bc7caac6310bfe9'
   config.port = 443
   config.secure = config.port == 443
   config.rescue_rake_exceptions = true
