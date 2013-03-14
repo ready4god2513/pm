6013bd7593dc115ad5a42159698ac661d9fcfcf8
Brandon Hansen <brandonh@ibethel.org>
2013-03-14 13:51:09 -0700

    New seed data with associations

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -6,12 +6,13 @@ describe Story do
     it { should validate_presence_of(:project) }
     it { should validate_presence_of(:pivotal_id) }
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:current_state) }
   end
   
   
   context "associations" do
     it { should belong_to(:project) }
+    it { should belong_to(:state) }
+    it { should belong_to(:story_type) }
     it { should belong_to(:iteration) }
     it { should have_many(:tasks) }
     it { should have_many(:attachments) }
